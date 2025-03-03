d497dc07d87c6f3933020372a4f0e4e2416d2008
Max Cunningham <maxzinedine@gmail.com>
2025-03-03 16:39:07 +0000

    delete tmp

--- tmp.py
+++
@@ -1,4 +0,0 @@
-import utils.file_structure as fs
-
-print(fs.generate_file_structure("env", "", "model", "", "exploration", ""))
-print(fs.generate_file_structure("env", "", "model2", "", "exploration", ""))
